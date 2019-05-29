623a75a499b111fa1c55403df4a7cc137f554358
Mario <krajacic.mario@gmail.com>
2019-05-29 18:03:07 +0200

    Adding background menu

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
